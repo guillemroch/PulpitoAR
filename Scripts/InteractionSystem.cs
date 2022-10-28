c61593e3146467505579d726ee1a0bdfbfe8d541
Alex Collado <alex.collado.gar@gmail.com>
2022-10-28 11:58:48 +0200

    Sistema de comida

--- Scripts/InteractionSystem.cs
+++ Scripts/InteractionSystem.cs
@@ -36,19 +36,31 @@ public class InteractionSystem : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit)){
             if (hit.collider != null) {
-                
-                Vector2 pet = inputs.Interaction.Pet.ReadValue<Vector2>();
-                if(pet[0] > 0 || pet[1] > 0){
-                    UnityEngine.Debug.Log("Petting");
-                    //if(playerAnimation.GetBool("isPoked") == false)
-                    playerAnimation.SetTrigger("IsPet");
-                    sw.Restart();
-                }
-                else{
-                    UnityEngine.Debug.Log(hit.collider);
-                    playerAnimation.SetTrigger("IsPoked");
-                    sw.Restart();
-                }    
+                if (hit.collider.gameObject.tag == "Player") {
+                    Vector2 pet = inputs.Interaction.Pet.ReadValue<Vector2>();
+                    if(pet[0] != 0 || pet[1] != 0){
+                        UnityEngine.Debug.Log("Petting");
+                        //if(playerAnimation.GetBool("isPoked") == false)
+                        playerAnimation.SetTrigger("IsPet");
+                        sw.Restart();
+                    }
+                    else{
+                        UnityEngine.Debug.Log(hit.collider);
+                        playerAnimation.SetTrigger("IsPoked");
+                        sw.Restart();
+                    }
+                }  
+            }
+        }
+
+        Ray ray2 = secondCamera.ScreenPointToRay(inputs.Interaction.Position.ReadValue<Vector2>());
+        RaycastHit hit2;
+        if (Physics.Raycast(ray2, out hit2)){
+            if (hit2.collider != null) {
+                if (hit2.collider.gameObject.tag == "Food") {
+                    UnityEngine.Debug.Log("Food");
+                    playerAnimation.SetTrigger("Eat");
+                }  
             }
         }
     }
@@ -60,7 +72,7 @@ public class InteractionSystem : MonoBehaviour
         double time = sw.Elapsed.TotalMilliseconds/1000;
         UnityEngine.Debug.Log($"Tiempo: {time} ms");
 
-        if(time >= 10){
+        if(time >= 10.0){
             sleep();
         }
     }
@@ -70,7 +82,9 @@ public class InteractionSystem : MonoBehaviour
     }
 
     private void sleep(){
-        playerAnimation.SetTrigger("Sleep");
+        if(playerAnimation != null)
+            playerAnimation.SetTrigger("Sleep");
+            UnityEngine.Debug.Log("Sleep");
     }
 
 }
